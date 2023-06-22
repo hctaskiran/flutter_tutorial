97f5cebb9b88c9f3999fb01e9b28e8bd3b4f69cf
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-06-22 13:37:14 +0300

    update

--- lib/202/tab_learn.dart
+++ lib/202/tab_learn.dart
@@ -7,27 +7,44 @@ class TabLearn extends StatefulWidget {
   State<TabLearn> createState() => _TabLearnState();
 }
 
-class _TabLearnState extends State<TabLearn> {
+class _TabLearnState extends State<TabLearn> with TickerProviderStateMixin{
+
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(length: 2, 
     child: Scaffold(
+      extendBody: true,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(onPressed:() {}),
+      floatingActionButton: FloatingActionButton(onPressed:() {
+        _tabController.animateTo(0);
+      }),
       bottomNavigationBar: BottomAppBar(
-        child: TabBar(tabs: [
+        notchMargin: 10,
+        shape: CircularNotchedRectangle(),
+        child: TabBar(
+          labelColor: Colors.pink.shade300,
+          indicatorColor: Colors.white,
+          // isScrollable: true,
+          controller: _tabController,
+          tabs: [
           Tab(text: "Page 1"),
-          Tab(text: "Page 2")
+          Tab(text: "Page 2"),
         ]),
       ),
-      appBar: AppBar(
-        bottom: TabBar(tabs: [
-          Tab(text: "Page 1"),
-          Tab(text: "Page 2")
-        ]),
-      ),
-      
-      body: TabBarView(children: [
+      appBar: AppBar(),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _tabController,
+        children: [
         Container(color: Colors.red,),
         Container(color: Colors.green),
       ])
