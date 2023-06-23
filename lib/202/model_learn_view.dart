102ce8f233bc7737e2ffa4cf0d404a7c4f336e5f
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-06-23 22:26:57 +0300

    update

---
+++ lib/202/model_learn_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/202/model_learn.dart';
+class ModelLearnView extends StatefulWidget {
+  const ModelLearnView({super.key});
+
+  @override
+  State<ModelLearnView> createState() => _ModelLearnViewState();
+}
+
+class _ModelLearnViewState extends State<ModelLearnView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    final user1 = PostModel();
+    user1.body = "hello";
+
+    final user2 = PostModel2(1, 2, "a", "b");
+
+    final user3 = PostModel3(1, 2, "a", "b");
+
+    final user4 = PostModel4(userID: 1, id: 2, title: "a", body: "b");
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
