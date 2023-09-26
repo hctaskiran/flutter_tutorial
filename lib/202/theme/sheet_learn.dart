52c5f105624aaf5ea6510cd3e4a77cba401d11db
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2023-09-26 21:22:26 +0300

    update

--- lib/202/theme/sheet_learn.dart
+++ lib/202/theme/sheet_learn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/202/image_learn_202.dart';
 
 class SheetLearn extends StatefulWidget {
   const SheetLearn({super.key});
@@ -7,34 +8,37 @@ class SheetLearn extends StatefulWidget {
   State<SheetLearn> createState() => _SheetLearnState();
 }
 
-class _SheetLearnState extends State<SheetLearn> {
+class _SheetLearnState extends State<SheetLearn> with ProductSheet {
   Color _backgroundColor = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Colors.cyan,
+      body: Center(
+        child: TextButton(onPressed: () {
+          showCustomSheet(context, ImageLearn202());
+        }, child: Text('Show')),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final result = await showModalBottomSheet(
-            backgroundColor: Colors.white,
-            barrierColor: Colors.transparent,
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20))
-            ),
-            context: context, 
-            builder: (context) {
-              return _CustomSheet(backgroundColor: _backgroundColor);
-            });
-            if (result is bool) {
-              setState(() {
-                _backgroundColor = Colors.purple;
+              backgroundColor: Colors.white,
+              barrierColor: Colors.transparent,
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+              context: context,
+              builder: (context) {
+                return _CustomSheet(backgroundColor: _backgroundColor);
               });
-            }
+          if (result is bool) {
+            setState(() {
+              _backgroundColor = Colors.purple;
+            });
+          }
         },
         child: Icon(Icons.share),
-        ),
+      ),
     );
   }
 }
@@ -51,6 +55,7 @@ class _CustomSheet extends StatefulWidget {
 
 class _CustomSheetState extends State<_CustomSheet> {
   Color _backgroundColor = Colors.white;
+  final _gripHeight = 30.0;
 
   @override
   Widget build(BuildContext context) {
@@ -60,24 +65,110 @@ class _CustomSheetState extends State<_CustomSheet> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Divider(
-              color: Colors.black26, 
-              thickness: 3, 
-              indent: MediaQuery.of(context).size.width*0.45, 
-              endIndent: MediaQuery.of(context).size.width*0.45
+            SizedBox(
+              height: _gripHeight,
+              child: Stack(
+                children: [
+                  Divider(
+                      color: Colors.black26,
+                      thickness: 3,
+                      indent: MediaQuery.of(context).size.width * 0.45,
+                      endIndent: MediaQuery.of(context).size.width * 0.45),
+                  Positioned(
+                    right: 0,
+                    height: 10,
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.close)),
+                  )
+                ],
+              ),
             ),
             Text('data'),
             Image.network('https://picsum.photos/200/300', height: 200),
             ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _backgroundColor = Colors.amber;
-                });
-                Navigator.of(context).pop<bool>(true);
-              }, 
-              child: Text('ok'))
+                onPressed: () {
+                  setState(() {
+                    _backgroundColor = Colors.amber;
+                  });
+                  Navigator.of(context).pop<bool>(true);
+                },
+                child: Text('ok'))
           ],
         ),
+      ),
+    );
+  }
+}
+
+mixin ProductSheet {
+  Future<T?> showCustomSheet<T>(BuildContext context, Widget child) async {
+    return showModalBottomSheet<T>(
+        backgroundColor: Colors.white,
+        barrierColor: Colors.transparent,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+        context: context,
+        builder: (context) {
+          return _CustomMainSheet(child: child);
+        });
+  }
+}
+
+
+class _CustomMainSheet extends StatelessWidget {
+  const _CustomMainSheet({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Color _backgroundColor = Colors.white;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _BaseSheetHeader(),
+            Expanded(child: child,)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BaseSheetHeader extends StatelessWidget {
+  const _BaseSheetHeader({
+    Key? key,
+  }) : _gripHeight = 30,
+    super(key: key);
+
+    final double _gripHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _gripHeight,
+      child: Stack(
+        children: [
+          Divider(
+              color: Colors.black26,
+              thickness: 3,
+              indent: MediaQuery.of(context).size.width * 0.45,
+              endIndent: MediaQuery.of(context).size.width * 0.45),
+          Positioned(
+            right: 0,
+            height: 10,
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close)),
+          )
+        ],
       ),
     );
   }
